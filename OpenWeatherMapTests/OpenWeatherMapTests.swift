ad395adcb7925282b117ad2b8fc33c458f12d1f7
Diego Bezerra <diego.bezerra21@gmail.com>
2017-07-02 10:22:25 -0300

    Adding tests

--- OpenWeatherMapTests/OpenWeatherMapTests.swift
+++ OpenWeatherMapTests/OpenWeatherMapTests.swift
@@ -11,26 +11,118 @@ import XCTest
 
 class OpenWeatherMapTests: XCTestCase {
     
+    var baseSessionManager:OWBaseSessionManager!
+    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        baseSessionManager = OWBaseSessionManager()
+        UserDefaults.standard.removeObject(forKey: "showtutorialkey")
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        baseSessionManager = nil
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testSetUserTutorialPreference() {
+        let msg = "Value returned from tutorial user preference is wrong"
+        
+        OWUserPreferences.setShowTutorialKey(true)
+        var value = OWUserPreferences.getShowTutorialKey()
+        
+        XCTAssertTrue(value, msg)
+        
+        OWUserPreferences.setShowTutorialKey(false)
+        value = OWUserPreferences.getShowTutorialKey()
+        
+        XCTAssertFalse(value, msg)
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testValidCallToWeatherGetData() {
+        
+        let url = "http://api.openweathermap.org/data/2.5/find?lat=-8.058714&lon=-34.872348&cnt=15&APPID=b7032a789ddf76625c8d9fe3efdf6129"
+        
+        let promise = expectation(description: "Request completed")
+        var statusCode:Int?
+        var responseError:Error?
+        
+        baseSessionManager.getWithParameters(nil, url: url) { (task, response, error) in
+            
+            statusCode = (task?.response! as? HTTPURLResponse)?.statusCode
+            responseError = error
+            
+            promise.fulfill()
         }
+        
+        waitForExpectations(timeout: 5, handler: nil)
+        XCTAssertNil(responseError, "Request returned an error! error: \(responseError?.localizedDescription ?? "")")
+        XCTAssertEqual(statusCode, 200, "StatusCode is wrong")
     }
+    
+    func testWeatherDataJsonParssing() {
+        
+        let url = "http://api.openweathermap.org/data/2.5/find?lat=-8.058714&lon=-34.872348&cnt=15&APPID=b7032a789ddf76625c8d9fe3efdf6129"
+        
+        let promise = expectation(description: "Request completed")
+        var jsonError:Error?
+        var weatherData:WeatherResponse?
+        
+        baseSessionManager.getWithParameters(nil, url: url) { (task, response, error) in
+            do {
+                try weatherData = WeatherResponse(dictionary: response as! [AnyHashable : Any])
+            } catch {
+                jsonError = error
+            }
+            
+            promise.fulfill()
+        }
+        
+        waitForExpectations(timeout: 5, handler: nil)
+        
+        XCTAssertNil(jsonError, "Json data was not passed. JsonError: \(jsonError?.localizedDescription ?? "")")
+        XCTAssertNotNil(weatherData)
+    }
+    
+    func testValidWeatherDataContent() {
+        let url = "http://api.openweathermap.org/data/2.5/find?lat=-8.058714&lon=-34.872348&cnt=15&APPID=b7032a789ddf76625c8d9fe3efdf6129"
+        
+        let promise = expectation(description: "Request completed")
+        var jsonError:Error?
+        var weatherData:WeatherResponse?
+        var city = CityVO()
+        var max:Double = 0
+        var min:Double = 0
+        var weatherDesc = ""
+        
+        baseSessionManager.getWithParameters(nil, url: url) { (task, response, error) in
+            do {
+                try weatherData = WeatherResponse(dictionary: response as! [AnyHashable : Any])
+                if let c = weatherData?.list.first as? CityVO {
+                    city = c
+                    max = c.main.temp_max.doubleValue
+                    min = c.main.temp_min.doubleValue
+                    weatherDesc = (c.weather.first as? WeatherVO)?.mDescription ?? ""
+                }
+            } catch {
+                jsonError = error
+            }
+            
+            promise.fulfill()
+        }
+        
+        waitForExpectations(timeout: 5, handler: nil)
+        
+        XCTAssertNil(jsonError, "Json data was not passed. JsonError: \(jsonError?.localizedDescription ?? "")")
+        XCTAssertNotNil(weatherData)
+        
+        XCTAssertEqual(city.name, "Recife", "City name not equals")
+        //This test is in Recife. It's impossible to have max and min temperatures under zero
+        XCTAssertGreaterThan(max, 0)
+        XCTAssertGreaterThan(min, 0)
+        XCTAssertFalse(weatherDesc.isEmpty, "Weahter description is empty")
+        
+    }        
     
 }
